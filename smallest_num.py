d5daf1ecd1db69ffcebfe41d3f428e72cd0b8a87
rupayanr <45901671+rupayanr@users.noreply.github.com>
2020-10-29 14:20:27 +0530

    Update smallest_num.py

--- smallest_num.py
+++ smallest_num.py
@@ -2,20 +2,23 @@
 input_list = input("Enter list elements to be inserted: ")
 print("\n")
 print("Finding smallest element......")
+
+# Splitting input string using space as delimiter 
 input_list = input_list.split()
 main_list = []
 
+# Typecasting and appending string numbers to main list 
 for val in input_list:
     val = int(val)
     main_list.append(val)
-print(main_list)
+
+# Initializing smallest variable with the first values of main list 
 smallest = main_list[0]
+
+# Comparing and changing values of smallest
 for val in main_list:
-   
-    if smallest > val:
-    
+    if smallest > val:    
         smallest = val
      
-
 print("Smallest no. is: ",smallest)    
 
